77a292685f8cbdc290b6f3d54be596835465b59b
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-07-29 12:22:17 +0530

    Add files via upload

---
+++ 2.converstion.js
@@ -0,0 +1,47 @@
+//Type convertion
+//1
+// implicite
+// let a=("22"+"22")
+// console.log(a)
+
+//2(number to string convertion)
+// let a=22
+// a=String(a);
+// console.log(typeof a)
+
+//3(boolean to string convert)
+// let a=true;
+// console.log( typeof a)
+// console.log(a)
+// a=String(a)
+// console.log(a)
+// console.log( typeof a)
+
+//4( string to number convertion)
+// let a="22"
+// a=Number(a);
+// console.log(typeof a)
+
+//5(string to boolean convert)
+// let a=true;
+// console.log( typeof a)
+// console.log(a)
+// a=String(a)
+// console.log(a)
+// console.log( typeof a)
+
+//6(number to boolean convert)
+// let a=22;
+// console.log( typeof a)
+// console.log(a)
+// a=Boolean(a)
+// console.log(a)
+// console.log( typeof a)
+
+//6(boolean to number convert)
+// let a=true;
+// console.log( typeof a)
+// console.log(a)
+// a=Number(a)
+// console.log(a)
+// console.log( typeof a)
