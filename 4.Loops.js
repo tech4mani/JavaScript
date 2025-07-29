147f7205843774fd0332128e001d464fd16a2d0e
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-07-29 12:25:14 +0530

    Add files via upload

---
+++ 4.Loops.js
@@ -0,0 +1,40 @@
+//sum in while
+// let n=5;
+// let a=1;
+// let i=1;
+
+// while(i<=n){
+//     a+=i;
+//     i++;
+// }
+// console.log(a)
+
+//sum in for
+// let a=5;
+// let b=1;
+// for (let i=1; i<=a; i++){
+//     b+=i;
+// }
+// console.log(b);
+
+// 1 to 5 while
+
+// let num=1;while (num<=5){
+//     console.log(num);
+//     num++
+// }
+// 1 to 5 do while
+
+// let num=1;
+// do{
+//     console.log(num);
+//     num++;
+// }
+// while(num <=5);
+// 1 to 5 for
+// for (let num=1;
+//      num<=5;
+//      num++
+//     ){
+//     console.log(num);
+// }
