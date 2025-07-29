37fc0b6f49ace9d1f824878b3821fb4f7ac4ed64
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-07-29 12:24:52 +0530

    Add files via upload

---
+++ 3.unary_binary.js
@@ -0,0 +1,29 @@
+//unary
+// let a = "10";
+// console.log(+a);
+// console.log(-a); 
+
+//1
+// let a="20";
+// a=--a;
+// console.log(a);
+
+//2
+// let a="10";
+// b="11";
+// c=+a + +b;
+// console.log(c);
+
+//4
+//binary
+// let x=10;
+// y=12;
+// z=x+y;
+// console.log(z)
+
+//4
+//concatenation
+// let a='22'
+// b='22'
+// c= a + b;
+// console.log(c)
