0991c60eaf31094d16a323fb87c5985dcede3bed
Manikandan M <146935225+tech4mani@users.noreply.github.com>
2025-07-30 11:12:33 +0530

    Update 1.Missness.js

--- 1.Missness.js
+++ 1.Missness.js
@@ -1,26 +1,26 @@
 // Missness value
 // boolean
 //1
-//  let a=true
-//  console.log(typeof a)
-//  console.log(a)
+ let a=true
+ console.log(typeof a)
+ console.log(a)
 
 //2
 //null
-//  let a=null
-//  console.log(typeof a)
-//  console.log(a)
+ let a=null
+ console.log(typeof a)
+ console.log(a)
 
 // 3
-// undefined
-// let a;
-// console.log(typeof a)
+undefined
+let a;
+console.log(typeof a)
 
 // 4
 // dynamic
-// let a="hii"
-// console.log(typeof a)
-// console.log(a)
-// a=123
-// console.log(typeof a)
-// console.log(a)
+let a="hii"
+console.log(typeof a)
+console.log(a)
+a=123
+console.log(typeof a)
+console.log(a)
